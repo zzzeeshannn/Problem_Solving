02b282086d3027e37b09eedeb6e259e65244976a
zzzeeshannn <zeeshanshaikh1205@gmail.com>
2021-06-04 13:12:01 -0700

    Problem 1 updated

---
+++ arrays/max_ones.cpp
@@ -0,0 +1,35 @@
+//
+// Created by zee on 6/4/21.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+// This was the fastest method at the time
+// Another possible approach is the sliding window approach but it is comparatively slower
+int findMaxConsecutiveOnes(vector<int>& nums) {
+    int count = 0;
+    int output = 0;
+    for(auto &val: nums){
+        if(val){
+            count++;
+        }
+        else{
+            output = max(output, count);
+            count = 0;
+        }
+    }
+    output = max(output, count);
+    return output;
+}
+
+int main(){
+    // Defining the array here
+    std::vector<int> defaultArray;
+    defaultArray = {1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1};
+
+    int output = findMaxConsecutiveOnes(defaultArray);
+
+    std::cout << "The maximum number of ones in a row are: " << output << std::endl;
+}
